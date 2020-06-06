c8c2848bbb05f2df3ebc9f9dba771e96acacf780
fatmajaafar <60942453+fatmajaafar@users.noreply.github.com>
2020-06-06 09:20:03 +0100

    re-design homepage

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
-import { ChartsModule, Label } from 'ng2-charts';
+import { ChartsModule, SingleDataSet, Label } from 'ng2-charts';
 import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 
@@ -38,6 +38,37 @@ export class HomeComponent implements OnInit, OnDestroy {
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
   ];
 
+  /**!!!!!!!!!!!!!**/
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'top'
+    },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: number[] = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [pluginDataLabels];
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)']
+    }
+  ];
+  /** */
+
+  public polarAreaChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+  public polarAreaChartData: SingleDataSet = [300, 500, 100, 40, 120];
+  public polarAreaLegend = true;
+
+  public polarAreaChartType: ChartType = 'polarArea';
+  /** */
   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
   ngOnInit(): void {
@@ -57,4 +88,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.authSubscription.unsubscribe();
     }
   }
+  // events
+
+  /* changeLabels() {
+    const words = ['hen', 'variable', 'embryo', 'instal', 'pleasant', 'physical', 'bomber', 'army', 'add', 'film',
+      'conductor', 'comfortable', 'flourish', 'establish', 'circumstance', 'chimney', 'crack', 'hall', 'energy',
+      'treat', 'window', 'shareholder', 'division', 'disk', 'temptation', 'chord', 'left', 'hospital', 'beef',
+      'patrol', 'satisfied', 'academy', 'acceptance', 'ivory', 'aquarium', 'building', 'store', 'replace', 'language',
+      'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny'];
+    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
+    this.pieChartLabels = Array.apply(null, { length: 3 }).map(_ => randomWord());
+  }*/
+
+  /* addSlice() {
+    this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
+    this.pieChartData.push(400);
+    this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
+  }
+
+  removeSlice() {
+    this.pieChartLabels.pop();
+    this.pieChartData.pop();
+    this.pieChartColors[0].backgroundColor.pop();
+  }*/
 }
