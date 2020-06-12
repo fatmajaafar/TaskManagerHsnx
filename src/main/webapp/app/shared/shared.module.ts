db57addfe36ae614fd0fe8d43b2df2638d4d488f
fatmajaafar <60942453+fatmajaafar@users.noreply.github.com>
2020-06-12 09:35:37 +0100

    added material deisgn

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+import { DemoMaterialModule } from './material-module';
 
 @NgModule({
-  imports: [TaskManagerHsnxSharedLibsModule],
+  imports: [TaskManagerHsnxSharedLibsModule, DemoMaterialModule],
   declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
   entryComponents: [LoginModalComponent],
   exports: [
+    DemoMaterialModule,
     TaskManagerHsnxSharedLibsModule,
     FindLanguageFromKeyPipe,
     AlertComponent,
