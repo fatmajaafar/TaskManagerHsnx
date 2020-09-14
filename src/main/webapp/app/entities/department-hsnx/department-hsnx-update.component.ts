a8ed6f373896f94026f7580204a2733f84e0a4e1
fatmajaafar <60942453+fatmajaafar@users.noreply.github.com>
2020-09-14 16:55:30 +0100

    add fields and translation to tblDepartment

--- src/main/webapp/app/entities/department-hsnx/department-hsnx-update.component.ts
+++ src/main/webapp/app/entities/department-hsnx/department-hsnx-update.component.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
 
 import { IDepartmentHsnx, DepartmentHsnx } from 'app/shared/model/department-hsnx.model';
 import { DepartmentHsnxService } from './department-hsnx.service';
+import { ICountryHsnx } from 'app/shared/model/country-hsnx.model';
+import { CountryHsnxService } from '../country-hsnx/country-hsnx.service';
 
 @Component({
   selector: 'jhi-department-hsnx-update',
@@ -14,26 +16,35 @@ import { DepartmentHsnxService } from './department-hsnx.service';
 })
 export class DepartmentHsnxUpdateComponent implements OnInit {
   isSaving = false;
-
+  countries: ICountryHsnx[] = [];
   editForm = this.fb.group({
     id: [],
     deptName: [null, [Validators.required]],
-    deptNote: []
+    deptNote: [],
+    tblCountryId: []
   });
 
-  constructor(protected departmentService: DepartmentHsnxService, protected activatedRoute: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(
+    protected departmentService: DepartmentHsnxService,
+    protected activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+    protected CountryService: CountryHsnxService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ department }) => {
       this.updateForm(department);
     });
+
+    this.CountryService.query().subscribe((res: HttpResponse<ICountryHsnx[]>) => (this.countries = res.body || []));
   }
 
   updateForm(department: IDepartmentHsnx): void {
     this.editForm.patchValue({
       id: department.id,
       deptName: department.deptName,
-      deptNote: department.deptNote
+      deptNote: department.deptNote,
+      tblCountryId: department.tblCountryId
     });
   }
 
@@ -56,7 +67,8 @@ export class DepartmentHsnxUpdateComponent implements OnInit {
       ...new DepartmentHsnx(),
       id: this.editForm.get(['id'])!.value,
       deptName: this.editForm.get(['deptName'])!.value,
-      deptNote: this.editForm.get(['deptNote'])!.value
+      deptNote: this.editForm.get(['deptNote'])!.value,
+      tblCountryId: this.editForm.get(['tblCountryId'])!.value
     };
   }
 
